c812c195299b5282b72014053c49befb2198fc72
Keventon <kevertonguimaraes19@gmail.com>
2023-12-26 16:24:57 -0300

    continuando modificaçÕes

---
+++ app/src/main/java/br/com/appco/copiadordecodigos/activity/Cadastro2Activity.java
@@ -0,0 +1,16 @@
+package br.com.appco.copiadordecodigos.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.appco.copiadordecodigos.R;
+
+public class Cadastro2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro2);
+    }
+}
